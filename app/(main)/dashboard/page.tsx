1e6dea87691340dbad526858ced64661b329d208
ZHAO JIAYI <z261459808@gmail.com>
2026-03-22 21:52:22 +0900

    feat: ProblemCardを追加

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,16 +1,14 @@
 import FilterBar from "@/components/dashboard/FilterBar";
-import ProblemCard from "@/components/dashboard/ProblemCard";
 import ProblemList from "@/components/dashboard/ProblemList";
 import WelcomeSection from "@/components/dashboard/WelcomeSection";
 
 const DashboardPage = () => {
   return (
-    <>
+    <div className="flex flex-col gap-4 m-20">
       <WelcomeSection />
       <FilterBar />
-      <ProblemCard />
       <ProblemList />
-    </>
+    </div>
   );
 };
 
